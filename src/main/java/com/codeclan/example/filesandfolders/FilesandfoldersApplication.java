d6bb72cb0247fd92a86c295ec978db9b77a7f455
Jen Merritt <jenjomez@gmail.com>
2020-02-25 19:24:24 +0000

    setup project

---
+++ src/main/java/com/codeclan/example/filesandfolders/FilesandfoldersApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filesandfolders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesandfoldersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesandfoldersApplication.class, args);
+	}
+
+}
